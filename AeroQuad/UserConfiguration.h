1317eb9da9a7f559dc68bba460cfaac45acd5df4
jcleve <john.cleve@peoplefluent.com>
2015-02-18 11:50:56 -0500

    disable mavlink telemetry

--- AeroQuad/UserConfiguration.h
+++ AeroQuad/UserConfiguration.h
@@ -152,13 +152,13 @@
 // Optional telemetry (for debug or ground station tracking purposes)
 // For more information on how to setup Telemetry please refer to http://aeroquad.com/showwiki.php?title=Wireless+Connection
 // *******************************************************************************************************************************
-#define WirelessTelemetry	// Enables Wireless telemetry on Serial3  // Wireless telemetry enable
+//#define WirelessTelemetry	// Enables Wireless telemetry on Serial3  // Wireless telemetry enable
 
-#define MavLink               // Enables the MavLink protocol
+//#define MavLink               // Enables the MavLink protocol
 //#define MAV_SYSTEM_ID 100		// Needs to be enabled when using MavLink, used to identify each of your copters using MavLink
 								// If you've only got one, leave the default value unchanged, otherwise make sure that each copter has a different ID 
 
-#define CONFIG_BAUDRATE 57600 // overrides default baudrate for serial port (Configurator/MavLink/WirelessTelemetry)
+//#define CONFIG_BAUDRATE 57600 // overrides default baudrate for serial port (Configurator/MavLink/WirelessTelemetry)
 
 //
 // *******************************************************************************************************************************
